8f2e3a622018b483ac4737974013416a118861f5
lizashaab <93035171+lizashaab@users.noreply.github.com>
2021-10-23 19:03:17 +0300

    Add files via upload

---
+++ сумматор чисел3.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	double a, b;
+	cout << "Vvedite pervoe chislo:";
+	cin >> a;
+	cout << "Vvedite vtoroe chislo:";
+	cin >> b;
+	double c = 0;
+	c = a + b;
+	cout << "Otvet:" << c << endl;
+	return 0;
+}
